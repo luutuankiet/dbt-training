c7f301ed1f7ccacf8782854338060ab2e8348c4e
Kiet Luu <kiet.lt@joonsolutions.com>
2025-01-26 09:49:34 +0000

    added price to tests

--- data-tests/all_product_price_greater_than_10_dollars.sql
+++ data-tests/all_product_price_greater_than_10_dollars.sql
@@ -2,6 +2,7 @@
 
 -- asumption : all upstream product prices was raised above 10$
 select name,
-        type 
+        type,
+        {{cents_to_dollar('price')}}
 from {{ source('ecom','raw_products') }} 
 where {{cents_to_dollars('price')}} < 10
