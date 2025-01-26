29225ea3bae891f50215bb041c9ba2a47badea3e
Kiet Luu <kiet.lt@joonsolutions.com>
2025-01-26 10:25:59 +0000

    dropped singular tests

--- data-tests/all_product_price_greater_than_10_dollars.sql
+++
@@ -1,8 +0,0 @@
-{{ config(severity='warn') }}
-
--- asumption : all upstream product prices was raised above 10$
-select name,
-        type,
-        {{cents_to_dollar('price')}}
-from {{ source('ecom','raw_products') }} 
-where {{cents_to_dollars('price')}} < 10
